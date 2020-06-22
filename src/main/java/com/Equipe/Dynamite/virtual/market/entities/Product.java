9854ae15a7190f1fa4296f8e6c6d616c22e9106f
O_o-X <O_o-X@Ban-Nox>
2020-06-22 09:23:59 -0500

    Create Emtities product

---
+++ src/main/java/com/Equipe/Dynamite/virtual/market/entities/Product.java
@@ -0,0 +1,79 @@
+package com.Equipe.Dynamite.virtual.market.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private int stock;
+	
+	@Column(length = 50)
+	private String name;
+	
+	private String description;
+	
+	
+	private double price;
+	
+	public Product() {
+		
+	}
+
+	public Product(int id, int stock, String name, String description, double price) {
+		super();
+		this.id = id;
+		this.stock = stock;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
---
+++ src/main/java/com/Equipe/Dynamite/virtual/market/entities/package-info.java
@@ -0,0 +1,1 @@
+package com.Equipe.Dynamite.virtual.market.entities;
